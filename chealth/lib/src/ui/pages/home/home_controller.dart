417dc9c525ffcbecac1cbf2df7fb77316af2be01
charles <charles>
2024-03-11 16:28:54 +0000

    sdsds

--- chealth/lib/src/ui/pages/home/home_controller.dart
+++ chealth/lib/src/ui/pages/home/home_controller.dart
@@ -5,10 +5,10 @@ import 'package:permission_handler/permission_handler.dart';
 class HomeController {
   List<HealthDataPoint> _healthDataList = [];
   final types = dataTypesAndroid;
-
+  late HealthFactory health ;
   Future authorize() async {
     final permissions = types.map((e) => HealthDataAccess.READ_WRITE).toList();
-    HealthFactory health = HealthFactory(useHealthConnectIfAvailable: true);
+     health = HealthFactory(useHealthConnectIfAvailable: true);
 
     // Request access to the required data types
     await Permission.activityRecognition.request();
@@ -33,5 +33,42 @@ class HomeController {
     }
 
     print('authorized: $authorized');
+
+    if (authorized) {
+      fetchData();
+    }
   }
+
+    Future fetchData() async {
+ 
+
+    // get data within the last 24 hours
+    final now = DateTime.now();
+    final yesterday = now.subtract(Duration(hours: 24));
+
+    // Clear old data points
+    _healthDataList.clear();
+
+    try {
+      // fetch health data
+      List<HealthDataPoint> healthData =
+          await health.getHealthDataFromTypes(yesterday, now, types);
+      // save all the new data points (only the first 100)
+      _healthDataList.addAll(
+          (healthData.length < 100) ? healthData : healthData.sublist(0, 100));
+    } catch (error) {
+      print("Exception in getHealthDataFromTypes: $error");
+    }
+
+    // filter out duplicates
+    _healthDataList = HealthFactory.removeDuplicates(_healthDataList);
+
+    // print the results
+    _healthDataList.forEach((x) => print(x));
+
+    // update the UI to display the results
+   
+  }
+
+
 }
